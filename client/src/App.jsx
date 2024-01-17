911ad2162e5ffdc3971c4373c2fdc1c63f76a091
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-01-17 18:24:08 +0400

    Add editor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 import TwitterComponent from "./TwitterComponent";
 import WalletComponent from "./WalletComponent";
+import Editor from "./Editor";
 import "./App.css";
+
+const INITIAL_DATA = {
+  time: new Date().getTime(),
+  blocks: [
+    {
+      type: "header",
+      data: {
+        text: "",
+        level: 4,
+      },
+    },
+  ],
+};
 
 function App() {
   const [state, setState] = useState({
     twitterHandle: null,
     Wallet: null,
   });
+
+  const [data, setData] = useState(INITIAL_DATA);
 
   const updateState = (newState) => {
     setState((prevState) => ({ ...prevState, ...newState }));
@@ -22,7 +38,33 @@ function App() {
           <br />
           Try now ⬇
         </p>
+        <div
+          className="editor"
+          style={{
+            backgroundColor: "#f0f0f0",
+            height: "100px",
+            fontSize: "14px",
+            borderRadius: "8px",
+            color: "#3B3B3B",
+            fontWeight: "300",
+          }}
+        >
+          <Editor
+            data={data}
+            onChange={setData}
+            editorblock="editorjs-container"
+          />
+          {/* <button
+            className="savebtn"
+            onClick={() => {
+              alert(JSON.stringify(data));
+            }}
+          >
+            Save
+          </button> */}
+        </div>
         <br />
+
         <div>
           {/* <p>Twitter Handle: {state.twitterHandle || "Not set"}</p> */}
           {/* <p>Wallet: {state.Wallet || "Not set"}</p> */}
