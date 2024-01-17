81d79b7a7325b6009aace957ea78c429a30f5566
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-01-17 20:52:09 +0400

    Update editor

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { https: true },
   plugins: [react(), mkcert()],
-})
+  build: {
+    rollupOptions: {
+      external: ["@editorjs/editorjs"],
+    },
+  },
+});
